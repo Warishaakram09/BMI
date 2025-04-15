843e6ffec57e5380a2d89cf9526a9e824f5a4a80
Your Name <warishaakram2005@gmail.com>
2025-04-16 00:08:32 +0500

    updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("💪 Simple BMI Calculator")
 height = st.number_input("Enter your height (in meters)", min_value=0.0, format="%.2f")
 weight = st.number_input("Enter your weight (in kilograms)", min_value=0.0, format="%.2f")
 
-if st.button("Calculate BMI")
+if st.button("Calculate BMI"):
     if height > 0 and weight > 0:
         bmi = weight / (height ** 2)
         st.success(f"Your BMI is: {bmi:.2f}")
